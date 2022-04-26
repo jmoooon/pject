b3f3502c79223874001b051b4ec4dc046ae2baa9
JMJung <JMJung@DESKTOP-VQEL25K>
2022-04-26 15:29:45 +0900

    init

---
+++ src/main/java/com/online/pject/PjectApplication.java
@@ -0,0 +1,13 @@
+package com.online.pject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/pject/PjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.pject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
